ec3b77bbad1e413cb7f7a87e82ff58687ad95194
Brandon Horlacher <bhorlacher@ancestry.com>
2017-01-30 12:06:46 -0700

    Adding markers and animation to the map.

--- js/map_driver.js
+++ js/map_driver.js
@@ -1,67 +1,99 @@
-var map = function initMap() {
-        var uluru = {lat: 40.311893, lng: -111.7036667};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: uluru
-        });
-}
-
 var initialMarkers = [
   {
     name: "Test1",
-    label: "A",
-    lat: 40,
-    lon: -111
+    lat: 40.3,
+    lon: -111.5,
+    visible: true
   },
   {
     name: "Test2",
-    label: "B",
-    lat: 41,
-    lon: -111
+    lat: 40.3,
+    lon: -111.6,
+    visible: true
   },
   {
     name: "Test3",
-    label: "C",
-    lat: 42,
-    lon: -111
+    lat: 40.3,
+    lon: -111.7,
+    visible: true
   },
   {
     name: "Test4",
-    label: "D",
-    lat: 43,
-    lon: -111
+    lat: 40.3,
+    lon: -111.8,
+    visible: true
   },
-  {name: "Test5",
-    label: "E",
-    lat: 44,
-    lon: -111
+  {
+    name: "Test5",
+    lat: 40.3,
+    lon: -111.9,
+    visible: true
   }
 ];
 
+var map;
+var MarkerList = [];
+
+function initMap() {
+  var uluru = {lat: 40.311893, lng: -111.7036667};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: uluru
+  });
+
+  initialMarkers.forEach(function(MarkerItem) {
+    var marker = new google.maps.Marker({
+      position: {lat: parseFloat(MarkerItem.lat), lng: parseFloat(MarkerItem.lon)},
+      map: map,
+      animation: google.maps.Animation.DROP,
+    });
+
+    marker.addListener('click', function() {
+      if (marker.getAnimation() == null) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {marker.setAnimation(null);}, 1400);
+      } 
+      else {
+        marker.setAnimation(null);
+      }
+    });
+
+    MarkerItem.Marker = marker;
+
+    MarkerList.push(new MarkerModel(MarkerItem));
+  });
+
+  ko.applyBindings(new MapMarkerController());
+}
+
 var MarkerModel = function(data) {
   this.name = ko.observable(data.name);
-  this.label = ko.observable(data.label);
   this.lat = ko.observable(data.lat);
   this.lon = ko.observable(data.lon);
-  this.listText = ko.computed(function() {
-    return this.label() + " - " + this.name();
-  }, this)
+  this.visible = ko.observable(data.visible);
+  
+  this.Marker = data.Marker;
+
+  this.visibleOnMap = ko.computed(function() {
+    if(this.visible() == true)
+    {
+      this.Marker.setMap(map);
+    }
+    else
+    {
+      this.Marker.setMap(null);
+    }
+  }, this);
 }
 
 var MapMarkerController = function() {
   var self = this;
 
-  this.MarkerData = [];
-  this.Markers = [];
+  self.MarkerData = new ko.observableArray(MarkerList);
 
-  initialMarkers.forEach(function(MarkerItem) {
-    self.MarkerData.push(new MarkerModel(MarkerItem));
-  });
-
-  initialMarkers.forEach(function(marker) {
-    var newMarker = new google.Maps.Marker({position: {lat:  marker.lat, lng: marker.lon}, map: map});
-    self.Markers.push(newMarker);
-  })
 }
 
-ko.applyBindings(new MapMarkerController());
+
+var ViewModel = function() {
+
+}
